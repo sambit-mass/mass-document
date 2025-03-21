0ecb43e7edd3b235977dd82d003edeeaa2ef89b9
Developer <massssss@gmail.com>
2025-03-21 17:07:31 +0530

    sh file

---
+++ demo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir Bahubali-1
+touch Bahubali-1/kalkeya
+touch Bahubali-1/shivagamidied
+touch Bahubali-1/kattapaold
+touch Bahubali-1/shiva
+touch Bahubali-1/avanthika
+touch Bahubali-1/bhallaladevold
+
+mkdir Bahubali-2
+touch Bahubali-2/amerendrabahubali
+touch Bahubali-2/devasena
+touch Bahubali-2/bhalladevyoung
+touch Bahubali-2/kattapayoung
+touch Bahubali-2/shivaganideve
